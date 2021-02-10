a85f640af054fdfc433aa3f70b2b159e938739a5
Balqis Kinanti <S10204973@connect.np.edu.sg>
2021-02-11 03:32:35 +0800

    add back to top logic

---
+++ scripts/back-to-top.js
@@ -0,0 +1,16 @@
+$(window).scroll(function() {
+    var height = $(window).scrollTop();
+    if (height > 100) {
+        $('#back2Top').fadeIn();
+    } else {
+        $('#back2Top').fadeOut();
+    }
+});
+$(document).ready(function() {
+    $("#back2Top").click(function(event) {
+        event.preventDefault();
+        window.location.href = "about.html#content";
+        return false;
+    });
+
+});
