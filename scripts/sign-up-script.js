8dcbab31f463b8655ed154b0fddf7e9c77f48c7a
Balqis Kinanti <S10204973@connect.np.edu.sg>
2021-02-14 17:32:24 +0800

    add error message if username is taken

--- scripts/sign-up-script.js
+++ scripts/sign-up-script.js
@@ -3,38 +3,42 @@ $(document).ready(function () {
 
     // update RESTDB if user submit
     $("#signup-submit").on("click", function (e) {
-        e.preventDefault();
+          e.preventDefault();
+          // user input
+          let username = $("#usernameInput").val();
+          let password = $("#passwordInput").val();
 
-        // user input
-        let username = $("#usernameInput").val();
-        let password = $("#passwordInput").val();
-
-        // update RESTDB
-        const APIKEY = "600e2fe91346a1524ff12dbd";
-        let jsondata = {
-          "username": username,
-          "password": password,
-          "points":0,
-          "darkmode":false
-        };
-        let settings = {
-          "async": true,
-          "crossDomain": true,
-          "url": "https://idassignment3-e0c6.restdb.io/rest/userdata",
-          "method": "POST", 
-          "headers": {
-            "content-type": "application/json",
-            "x-apikey": APIKEY,
-            "cache-control": "no-cache"
-          },
-          "processData": false,
-          "data": JSON.stringify(jsondata),
-          "beforeSend": function(){
-            $("#login-submit").prop( "disabled", true);
-            $("#add-login-form").trigger("reset");
+          // update RESTDB
+          const APIKEY = "600e2fe91346a1524ff12dbd";
+          let jsondata = {
+            "username": username,
+            "password": password,
+            "points":0,
+            "darkmode":false
+          };
+          let settings = {
+            "async": true,
+            "crossDomain": true,
+            "url": "https://idassignment3-e0c6.restdb.io/rest/userdata",
+            "method": "POST", 
+            "headers": {
+              "content-type": "application/json",
+              "x-apikey": APIKEY,
+              "cache-control": "no-cache"
+            },
+            "processData": false,
+            "data": JSON.stringify(jsondata),
+            "beforeSend": function(){
+              $("#login-submit").prop( "disabled", true);
+              $("#add-login-form").trigger("reset");
+            },
+            //check if username is taken . username must be unique
+            "error": function (xhr, status, errorThrown) { 
+              alert('Username taken. Plase use another one :)');
+              console.clear();
           }
         } 
-        
+
         //show successful message
         $.ajax(settings).done(function (response) {
           $(".alert").show();
