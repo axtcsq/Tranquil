5a322de2c330bd72ad7cbaf29437d76d8c93cae6
Balqis Kinanti <S10204973@connect.np.edu.sg>
2021-02-16 03:20:42 +0800

    fix bug of loader displaying to long, not hiding elements

--- scripts/loader-script.js
+++ scripts/loader-script.js
@@ -1,18 +1,14 @@
-$(function() {
-    $(window).bind("load", function() {
-        let spinnerWrapper = document.querySelector('.spinner-wrapper');
+// jshint esversion:6
+let spinnerWrapper = document.querySelector('.spinner-wrapper');
 
-        // show content after loading is up
-        function loadAd(){
-            spinnerWrapper.parentElement.removeChild(spinnerWrapper);
-            $("#content").show();
-        }
-    
-        // show loading animation for 2000 ms and hide content
-        $(document).ready(function(e) {
-            setTimeout(loadAd,2000);
-            $("#content").hide();
-        });
-    });
-});
+// show content after loading is up
+function loadAd(){
+    spinnerWrapper.parentElement.removeChild(spinnerWrapper);
+    $("#content").show();
+}
+
+// show loading animation for 2000 ms and hide content
+setTimeout(loadAd,500);
+$("#content").hide();
+
     
